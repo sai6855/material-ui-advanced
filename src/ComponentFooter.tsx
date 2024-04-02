73d0b8bd433f76a444a566fff81f0a9995700218
sai6855 <saichand685@gmail.com>
2024-04-02 19:41:57 +0530

    add style

--- src/ComponentFooter.tsx
+++ src/ComponentFooter.tsx
@@ -1,15 +1,31 @@
 "use client";
-import { Button } from "@mui/material";
+import * as React from "react";
+import { Button, Snackbar } from "@mui/material";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 
 function ComponentFooter({ content }: { content: string }) {
+  const [open, setOpen] = React.useState(false);
+
   return (
-    <Button
-      onClick={() => {
-        navigator.clipboard.writeText(content);
-      }}
-    >
-      Copy code
-    </Button>
+    <>
+      <Button
+        onClick={() => {
+          navigator.clipboard.writeText(content);
+          setOpen(true);
+        }}
+        startIcon={<ContentCopyIcon fontSize={"small"} />}
+      >
+        Copy code
+      </Button>
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        open={open}
+        onClose={() => {
+          setOpen(false);
+        }}
+        message="Code copied to clipboard!"
+      />
+    </>
   );
 }
 
