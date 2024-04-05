273e6baff496f388a4c5b174a1a52afb19d79a61
sai6855 <saichand685@gmail.com>
2024-04-05 22:33:47 +0530

    Update page.tsx with component title and divider

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,24 @@ import DebouncedInput from "@/src/components/DebouncedInput";
 import OTPInput from "@/src/components/OtpInput";
 import InputFileUpload from "@/src/components/InputFileUpload";
 import SwipeableTabs from "@/src/components/SwipeableTabs";
-import { Box, SxProps } from "@mui/material";
+import { Box, Divider, SxProps } from "@mui/material";
 import fs from "node:fs";
 import ComponentFooter from "@/src/ComponentFooter";
+
+function Header({ title }: { title: string }) {
+  return <h2>{title}</h2>;
+}
 
 function ComponentWrapper({
   children,
   sx,
   path,
+  title,
 }: {
   children: React.ReactNode;
   sx?: SxProps;
   path: string;
+  title: string;
 }) {
   const content = fs
     .readFileSync(path, "utf-8")
@@ -24,7 +30,8 @@ function ComponentWrapper({
     .join("\n");
 
   return (
-    <Box sx={{ mx: ["1rem", "15rem"] }}>
+    <Box sx={{ mx: ["1rem", "15rem"], my: "1rem" }}>
+      <Header title={title} />
       <Box
         sx={{
           p: 2,
@@ -43,6 +50,7 @@ function ComponentWrapper({
         content={content}
         sourceCode={`https://github.com/sai6855/material-ui-advanced/blob/master/${path}`}
       />
+      <Divider sx={{ pt: "1rem" }} />
     </Box>
   );
 }
@@ -51,24 +59,32 @@ export default function Page() {
   return (
     <>
       <ComponentWrapper
+        title="Nested Menu"
         path="src/components/NestedMenu.tsx"
         sx={{ display: "grid", placeItems: "center" }}
       >
         <NestedMenu />
       </ComponentWrapper>
-      <ComponentWrapper path="src/components/DebouncedInput.tsx">
+      <ComponentWrapper
+        title="Debounced Input"
+        path="src/components/DebouncedInput.tsx"
+      >
         <DebouncedInput />
       </ComponentWrapper>
-      <ComponentWrapper path="src/components/OtpInput.tsx">
+      <ComponentWrapper title="OTP Input" path="src/components/OtpInput.tsx">
         <OTPInput />
       </ComponentWrapper>
       <ComponentWrapper
+        title="Input File Upload"
         sx={{ py: 6 }}
         path="src/components/InputFileUpload.tsx"
       >
         <InputFileUpload />
       </ComponentWrapper>
-      <ComponentWrapper path="src/components/SwipeableTabs.tsx">
+      <ComponentWrapper
+        title="Swipeable Tabs"
+        path="src/components/SwipeableTabs.tsx"
+      >
         <SwipeableTabs />
       </ComponentWrapper>
     </>
